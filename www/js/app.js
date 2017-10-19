1d2d4aec7bdff3cda7c49ff37f776322789e4810
jeffleus <jeffleus@gmail.com>
2017-10-18 21:52:07 -0700

    Init ionic driver app for cognate plugin

---
+++ www/js/app.js
@@ -0,0 +1,92 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+    if (window.MyCordovaPlugin) {
+		var cognitoIdentityUserPoolId = "us-west-2_KMI3gTfQw";
+		var cognitoIdentityUserPoolAppClientId = "49f7iepq786236nea8t33m1kje";
+		var cognitoArnIdentityPoolId = "us-west-2:28695927-b308-4073-acd6-fedc4e1cd40b";
+
+		var options = {
+			"CognitoIdentityUserPoolId": cognitoIdentityUserPoolId,
+			"CognitoIdentityUserPoolAppClientId": cognitoIdentityUserPoolAppClientId,
+			"arnIdentityPoolId": cognitoArnIdentityPoolId
+		};
+
+		console.log('MyCordovaPlugin: init the plugin after $ionicPlatform.ready()');
+		MyCordovaPlugin.init(options, function() {
+											console.log("connectionPluginInstance Init Ok");
+											//login();
+										}, function() {
+											console.log("connectionPluginInstance Init Fail");
+										});	
+	}
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.search', {
+    url: '/search',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/search.html'
+      }
+    }
+  })
+
+  .state('app.browse', {
+      url: '/browse',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/browse.html'
+        }
+      }
+    })
+    .state('app.playlists', {
+      url: '/playlists',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/playlists.html',
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+
+  .state('app.single', {
+    url: '/playlists/:playlistId',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/playlist.html',
+        controller: 'PlaylistCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/playlists');
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,74 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+	  
+	console.log('MyCordovaPlugin: start the login logic on the plugin...');
+	MyCordovaPlugin.login({"username":"fsdemo-manager", "password":"FuelStation17!"}, function(res) {
+								// Success								
+								console.log(res);
+								$ionicPopup.alert({
+									title: 'Login Success',
+									template: res
+								});
+							}, function(err) {
+								 // Error : err
+								 console.error(err);
+							 });
+
+//  	MyCordovaPlugin.getDate(function(res) {
+//							   console.log(res);
+//							   }, function(err) {
+//							   console.log(err);
+//							   });
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+	
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+});
